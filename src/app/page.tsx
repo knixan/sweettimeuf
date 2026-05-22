9600143de7db5004723c3deaac80ba8e46401cf1
Josefine <josefineeriksson@live.se>
2026-05-22 16:12:27 +0200

    fix: force dynamic rendering on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/site/Hero";
 import { PopularProducts } from "@/components/site/PopularProducts";
 
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return (
